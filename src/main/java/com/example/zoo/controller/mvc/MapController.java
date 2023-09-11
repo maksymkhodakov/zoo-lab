d44289e6673b087356c3976eef5b52edace15500
Jovakinn <max05012004@gmail.com>
2023-09-12 01:54:17 +0300

    fix severe security issue on prod

--- src/main/java/com/example/zoo/controller/mvc/MapController.java
+++ src/main/java/com/example/zoo/controller/mvc/MapController.java
@@ -5,6 +5,8 @@ import com.example.zoo.repository.CountryRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,12 +19,20 @@ import java.util.List;
 @RequestMapping("/map")
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class MapController {
+
+    /**
+     * Generated in Google console for 1$
+     */
+    @Value("${google.api.key}")
+    @NonFinal private String apiKey;
+
     CountryRepository countryRepository;
 
     @GetMapping("/all")
     public String coordinates(Model model) {
         List<Coordinates> coordinateList = countryRepository.getCoordinates();
         model.addAttribute("coordinates", coordinateList);
+        model.addAttribute("apiKey", apiKey);
         return "map";
     }
 }
