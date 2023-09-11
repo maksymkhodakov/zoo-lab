1cdf307524923989655287d46b1bce819fcfe3ad
Jovakinn <max05012004@gmail.com>
2023-09-11 19:46:42 +0300

    add Map on UI

--- src/main/java/com/example/zoo/repository/CountryRepository.java
+++ src/main/java/com/example/zoo/repository/CountryRepository.java
@@ -1,8 +1,13 @@
 package com.example.zoo.repository;
 
 import com.example.zoo.entity.Country;
+import com.example.zoo.integratons.maps.domain.dto.Coordinates;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CountryRepository extends JpaRepository<Country, Long> {
-
+    @Query("select c.coordinates from Country c where c.coordinates is not null")
+    List<Coordinates> getCoordinates();
 }
