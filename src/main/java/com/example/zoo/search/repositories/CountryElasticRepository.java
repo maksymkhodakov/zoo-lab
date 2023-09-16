693977a67ea7a82fcae15de43e77614fd349baad
Jovakinn <max05012004@gmail.com>
2023-09-17 02:45:49 +0300

    add country elasticsearch integration

--- src/main/java/com/example/zoo/search/repositories/CountryElasticRepository.java
+++ src/main/java/com/example/zoo/search/repositories/CountryElasticRepository.java
@@ -1,7 +1,16 @@
 package com.example.zoo.search.repositories;
 
 import com.example.zoo.search.dto.CountryElasticDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+import java.time.LocalDate;
+
 public interface CountryElasticRepository extends ElasticsearchRepository<CountryElasticDTO, Long> {
+    @Query("{\"match\": {\"continent\": {\"query\": \"?0\"}}}")
+    Page<CountryElasticDTO> findByContinent(String continent, Pageable pageable);
+
+    Page<CountryElasticDTO> findByCreateDateIsBetween(LocalDate start, LocalDate end, Pageable pageable);
 }
--- src/main/java/com/example/zoo/services/impl/CountryServiceImpl.java
+++ src/main/java/com/example/zoo/services/impl/CountryServiceImpl.java
@@ -3,11 +3,16 @@ package com.example.zoo.services.impl;
 import com.example.zoo.data.CountryData;
 import com.example.zoo.dto.CountryDTO;
 import com.example.zoo.dto.SearchDTO;
+import com.example.zoo.entity.Country;
+import com.example.zoo.enums.Continent;
 import com.example.zoo.exceptions.ApiErrors;
 import com.example.zoo.exceptions.OperationException;
 import com.example.zoo.integratons.maps.service.ContinentCoordinatesService;
 import com.example.zoo.mapper.CountryMapper;
 import com.example.zoo.repository.CountryRepository;
+import com.example.zoo.search.dto.AnimalElasticDTO;
+import com.example.zoo.search.dto.CountryElasticDTO;
+import com.example.zoo.search.repositories.CountryElasticRepository;
 import com.example.zoo.services.CountryService;
 import com.example.zoo.utils.SearchUtil;
 import lombok.AccessLevel;
@@ -20,6 +25,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 
 @Slf4j
@@ -28,6 +34,7 @@ import java.util.List;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class CountryServiceImpl implements CountryService {
     CountryRepository countryRepository;
+    CountryElasticRepository countryElasticRepository;
     ContinentCoordinatesService coordinatesService;
 
     @Override
@@ -36,6 +43,21 @@ public class CountryServiceImpl implements CountryService {
                 .stream()
                 .map(CountryMapper::entityToDto)
                 .toList();
+    }
+
+    @Override
+    public Page<CountryElasticDTO> getAllElastic(SearchDTO searchDTO) {
+        return countryElasticRepository.findAll(SearchUtil.getPageable(searchDTO));
+    }
+
+    @Override
+    public Page<CountryElasticDTO> getByContinent(String continent, SearchDTO searchDTO) {
+        return countryElasticRepository.findByContinent(continent, SearchUtil.getPageable(searchDTO));
+    }
+
+    @Override
+    public Page<CountryElasticDTO> getByDateRange(LocalDate start, LocalDate end, SearchDTO searchDTO) {
+        return countryElasticRepository.findByCreateDateIsBetween(start, end, SearchUtil.getPageable(searchDTO));
     }
 
     @Override
@@ -51,6 +73,14 @@ public class CountryServiceImpl implements CountryService {
         country.setCoordinates(coordinatesService.continentToCoordinates(countryData.getContinent()));
         countryRepository.saveAndFlush(country);
         log.info("Country with id: " + country.getId() + " created");
+        saveElastic(country);
+    }
+
+    @Transactional
+    public void saveElastic(Country country) {
+        final var countryElastic = CountryMapper.entityToElasticDTO(country);
+        countryElasticRepository.save(countryElastic);
+        log.info("Country with id " + country.getId() + " created in elasticsearch");
     }
 
     @Override
@@ -65,9 +95,25 @@ public class CountryServiceImpl implements CountryService {
     }
 
     @Override
+    @Transactional
+    public void updateElastic(Long id, CountryData countryData) {
+        final var country = getByIdElastic(id);
+        country.setName(countryData.getName());
+        country.setContinent(countryData.getContinent().getName());
+        country.setCoordinates(coordinatesService.continentToCoordinates(countryData.getContinent()).toString());
+        countryElasticRepository.save(country);
+    }
+
+    @Override
     public CountryDTO getById(Long id) {
         return CountryMapper.entityToDto(countryRepository.findById(id)
                 .orElseThrow(() -> new OperationException(ApiErrors.COUNTRY_NOT_FOUND)));
+    }
+
+    @Override
+    public CountryElasticDTO getByIdElastic(Long id) {
+        return countryElasticRepository.findById(id)
+                .orElseThrow(() -> new OperationException(ApiErrors.COUNTRY_ELASTIC_NOT_FOUND));
     }
 
     @Override
@@ -76,5 +122,11 @@ public class CountryServiceImpl implements CountryService {
         final var country = countryRepository.findById(id)
                 .orElseThrow(() -> new OperationException(ApiErrors.COUNTRY_NOT_FOUND));
         countryRepository.delete(country);
+    }
+
+    @Override
+    public void deleteElastic(Long id) {
+        final var country = getByIdElastic(id);
+        countryElasticRepository.save(country);
     }
 }
